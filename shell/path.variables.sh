3f8c414d390c8c33ca7655d0a48952a3c14f0f9b
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2016-12-22 16:23:46 +0100

    add shell variables and idea-options file

---
+++ shell/path.variables.sh
@@ -0,0 +1,36 @@
+# Path variables settings
+
+## Development directory
+export DEV_HOME=$HOME/Development
+export WORK_SPACE=$DEV_HOME/Workspace
+export SOFT_HOME=$DEV_HOME/Soft
+export DOWNLOADS_HOME=$DEV_HOME/Downloads
+
+## Ocasional user tools
+if [ -d "$HOME/bin" ] ; then
+    PATH=$HOME/bin:$PATH
+fi
+
+## Java
+export JAVA_HOME=$SOFT_HOME/jdk
+PATH=$PATH:$JAVA_HOME/bin
+
+## GO
+export GO_HOME=/urs/local/go
+export GOPATH=$WORK_SPACE/go
+PATH=$PATH:$GO_HOME/bin
+
+## Swift
+export SWIFT_HOME=$SOFT_HOME/swift
+PATH=$SWIFT_HOME/usr/bin:$PATH
+
+# Zookeeper
+export ZOOKEEPER_HOME=$SOFT_HOME/zookeeper
+PATH=$PATH:ZOOKEEPER_HOME
+
+## sdkman
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+## final export of PATH variable
+export PATH
